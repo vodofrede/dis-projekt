3cab1209cb45b6f1fe839e0584b9a2984cff504f
Frederik Palmø <frederik@palmoe.dk>
2023-06-07 13:50:08 +0200

    Co-authored-by: Mathias Brams-Larsen <MathiasBrams@users.noreply.github.com> Co-authored-by: MadsNoerregaard <MadsNoerregaard@users.noreply.github.com>

--- src/recipe.py
+++ src/recipe.py
@@ -1,3 +1,4 @@
+import model
 from flask import Blueprint, render_template
 
 recipe = Blueprint('recipe', __name__, url_prefix="/recipe")
@@ -6,29 +7,31 @@ recipe = Blueprint('recipe', __name__, url_prefix="/recipe")
 # get all recipes, filtered or not
 @recipe.route("/")
 def getAll(count): # define and include type on param?
-    recipes = selectAllRecipes(count)
+    recipes = model.Recipe.selectAllRecipes()
     return render_template('frontpage.html', title='Recipes', recipeList=recipes)
 
 # get all recipes, 
 @recipe.route("/")
-def get(): # define and include type on param?
-    recipes = selectAllRecipes()
+def getNRecipes(): # define and include type on param?
+    recipes = model.Recipe.getNRecipes()
     return render_template('frontpage.html', title='Recipes', recipeList=recipes)
-
-# search for recipes, based on ingredient filter (list of ingredients)
-@recipe.route("/filter")
-def filter(filter):
-    recipes = getRecipesByFilter(filter)
-    return render_template('frontpage.html', title='Search', recipeList=recipes)
-
-# search for recipe name, return list of hits
-@recipe.route("/search")
-def search(name):
-    recipes = searchRecipes(name)
-    return render_template('frontpage.html', title='Search', recipeList=recipes)
 
 # get recipe by id
 @recipe.route("/<int:id>")
 def getRecipe(id):
-    recipe = selectRecipeById(id)
+    recipe = model.Recipe.getRecipeById(id)
     return render_template('recipesite.html', title='Recipe', recipe=recipe)
+
+# search for recipes, based on ingredient filter (list of ingredients)
+# @recipe.route("/filter")
+# def filter(filter):
+#     recipes = getRecipesByFilter(filter)
+#     return render_template('frontpage.html', title='Search', recipeList=recipes)
+
+# search for recipe name, return list of hits
+# @recipe.route("/search")
+# def search(name):
+#     recipes = searchRecipes(name)
+#     return render_template('frontpage.html', title='Search', recipeList=recipes)
+
+
